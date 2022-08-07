a26571a9f03f2e335bed844b7c8d5b59f85fb4c0
TristanAly <tristan.aly@gmail.com>
2022-08-07 21:03:54 +0200

    Initial Commit

---
+++ BaKEggApp/BaKEggAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaKEggAppApp.swift
+//  BaKEggApp
+//
+//  Created by Tristan Aly on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BaKEggAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
